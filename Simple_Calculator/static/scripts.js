557d69217bc24fae0a093ed1db21ea3a24e6f897
nk112233 <nikunjkadu19@gmail.com>
2023-11-05 17:09:46 +0530

    Added History and various small improvements to CSS and functionalities

--- Simple_Calculator/static/scripts.js
+++ Simple_Calculator/static/scripts.js
@@ -1,20 +1,40 @@
+let history = {};
 function append(value){
     const display=document.getElementById("input");
-    display.textContent+=value;
+    let x = display.textContent;
+    let lc = x.slice(-1);
+    if(IsOperator(lc) && IsOperator(value)){
+            console.log(x.slice(0,-1));
+            display.textContent = x.slice(0,-1);
+            display.textContent += value;
+    }
+    else{
+        display.textContent+=value;
+    }
 
 }
-
+function IsOperator(strr){
+        const operators = [ "+", "-", "*", "/", "^"];
+        return operators.includes(strr);
+}
 function calc(){
     let x=document.getElementById("input").textContent;
+    document.getElementById("expr").textContent = x;
     x = x.replace("^","**");
     let y=eval(x);
     document.getElementById("input").textContent=y;
+    x = x.replace("**","^");
+    history[x] = y;
+    console.log(history);
 }
 
 function allclr() { 
     document.getElementById("input").textContent = "";
+    document.getElementById("expr").textContent = "";
 }
 function del() { 
+
+    document.getElementById("expr").textContent = "";
     const display = document.getElementById("input");
     display.textContent = display.textContent.slice(0, -1);
 }
@@ -27,16 +47,16 @@ function keyboard(e) {
     console.log(keyValue);
     if (/^[0-9\+\^\-\*\.\/]$/.test(keyValue)) {
         document.getElementById(keyValue).style.transform = `translateY(.125rem)`;
-        document.getElementById(keyValue).style.backgroundColor = `#805eb1`;
+        document.getElementById(keyValue).style.backgroundColor = `#57595b`;
         document.addEventListener('keyup', () => {
             document.getElementById(keyValue).style.transform = 'none';
-            document.getElementById(keyValue).style.backgroundColor = `#27005D`;
+            document.getElementById(keyValue).style.backgroundColor = `#111111`;
         });
         append(keyValue);
     } 
     else if (keyValue === "Enter") {
         document.getElementById("result").style.transform = `translateY(.125rem)`;
-        document.getElementById("result").style.backgroundColor = `#805eb1`;
+        document.getElementById("result").style.backgroundColor = `#57595b`;
         document.addEventListener('keyup', () => {
         document.getElementById("result").style.transform = 'none';
         document.getElementById("result").style.backgroundColor = `#4caf50`;
@@ -45,12 +65,29 @@ function keyboard(e) {
     }
     else if(keyValue === "Backspace"){
         document.getElementById("del").style.transform = `translateY(.125rem)`;
-        document.getElementById("del").style.backgroundColor = `#805eb1`;
+        document.getElementById("del").style.backgroundColor = `#57595b`;
         document.addEventListener('keyup', () => {
         document.getElementById("del").style.transform = 'none';
-        document.getElementById("del").style.backgroundColor = `#27005D`;
+        document.getElementById("del").style.backgroundColor = `#111111`;
         });
         const display = document.getElementById("input");
         display.textContent = display.textContent.slice(0, -1);
     }
 }
+
+function updateHistory() {
+    const historyList = document.getElementById("history-list");
+    historyList.innerHTML = ""; 
+    for (const key in history) {
+        const calculation = `<div class = "hsrecord">${key} = ${history[key]}</div>`;
+        historyList.innerHTML += calculation;
+    }
+}
+setInterval(updateHistory, 10);
+function clearHistory(){
+    history = {};
+    document.getElementById("history-list").innerHTML = "";
+}
+
+
+
